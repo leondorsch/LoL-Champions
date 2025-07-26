339c745a7306ebc60f3dd6c891567be2828eeb53
Leon Dorsch <leon.dorsch@gmx.de>
2025-07-26 15:22:19 +0200

    removed environment from gitignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig : {
+        apiKey: "AIzaSyBLm_gi226-JvNwZzHcXpjtUNYlOhh9UnA",
+        authDomain: "lol-champions-cbaee.firebaseapp.com",
+        projectId: "lol-champions-cbaee",
+        storageBucket: "lol-champions-cbaee.firebasestorage.app",
+        messagingSenderId: "359195088573",
+        appId: "1:359195088573:web:e6218b83ac583638ad09d4"
+    },
+} 
+
